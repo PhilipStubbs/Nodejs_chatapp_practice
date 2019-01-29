33b60145f2ea31790616bdd60ce4f50ff23feec9
PhilipStubbs <24510543+PhilipStubbs@users.noreply.github.com>
2019-01-29 15:26:24 +0200

    trying to remove users on logout

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server);
+	nicknames = [];
 
 server.listen(3000);
 
@@ -10,9 +11,30 @@ app.get('/', function(req, res) {
 });
 
 io.sockets.on('connection', function(socket) {
+	socket.on('new user', function(data, callback) {
+		if (nicknames.indexOf(data) != -1)
+		{
+			callback(false);
+		}
+		else
+		{
+			callback(true);
+			socket.nickname = data;
+			nicknames.push(socket.nickname);
+			io.sockets.emit('usernames', nicknames);
+		}
+	});
+
 	socket.on('send message', function(data) {
 		io.sockets.emit('new message', data);
 	});
+
+	socket.on('disconnect', function(data)
+	{
+		if (!socket.nickname) return;
+		nicknames.splice(nickname.indexOf(socket.nickname), 1);
+		
+	})
 });
 
 

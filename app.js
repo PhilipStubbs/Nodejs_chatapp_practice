200ec9a65128de4000bd17d44a8acf84b14343ed
PhilipStubbs <24510543+PhilipStubbs@users.noreply.github.com>
2019-01-30 14:12:30 +0200

    adding whisper

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server);
-	nicknames = [];
+	users = {};
 
 server.listen(3000);
 
@@ -12,28 +12,54 @@ app.get('/', function(req, res) {
 
 io.sockets.on('connection', function(socket) {
 	socket.on('new user', function(data, callback) {
-		if (nicknames.indexOf(data) != -1)
+		if (data in users)
 		{
 			callback(false);
 		}
 		else
 		{
+			
 			callback(true);
 			socket.nickname = data;
-			nicknames.push(socket.nickname);
-			io.sockets.emit('usernames', nicknames);
+			users[socket.nickname] = socket;
+			// io.sockets.emit('usernames', nicknames);
+			updateNicknames();
 		}
 	});
 
 	function updateNicknames(){
-		io.sockets.emit('usernames', nicknames);
+		io.sockets.emit('usernames', Object.keys(users));
 	}
 
-	socket.on('send message', function(data) {
-		var tmp;
-		tmp = socket.nickname + " : " + data;
-		// io.sockets.emit('new message', tmp);
+	socket.on('send message', function(data, callback) {
+		var msg = data.trim();
+		if (msg.substr(0,3) === '/w ' || msg.substr(0,3) === "\\w ")
+		{
+			msg = msg.substr(3);
+			var ind = msg.indexOf(' ');
+			if (ind !== -1){
+				var name = msg.substring(0, ind);
+				var msg = msg.substring(ind +1);
+				if(name in users){
+					console.log("Whisper");
+					users[name].emit('whisper', {msg : msg, nick : socket.nickname});
+				}
+				else
+				{
+					callback("Error! enter a vaild User");
+				}
+				
+			}
+			else
+			{
+				callback("Error! enter a message");
+			}
+		
+		}
+		else
+		{
 		io.sockets.emit('new message', {msg : data, nick : socket.nickname});
+		}
 	});
 
 	
@@ -44,7 +70,7 @@ io.sockets.on('connection', function(socket) {
 		{
 			return;
 		}
-		nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+		delete users[socket.nickname];
 		updateNicknames();
 	})
 
